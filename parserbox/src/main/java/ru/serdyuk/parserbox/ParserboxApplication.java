e40a51f9dd50336c8ffbdefa542b95a7f48eea16
Sergei <Serduyksb@gimai.com>
2021-12-22 17:44:52 +0300

    Init project

---
+++ parserbox/src/main/java/ru/serdyuk/parserbox/ParserboxApplication.java
@@ -0,0 +1,13 @@
+package ru.serdyuk.parserbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParserboxApplication.class, args);
+	}
+
+}
---
+++ parserbox/src/test/java/ru/serdyuk/parserbox/ParserboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serdyuk.parserbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
