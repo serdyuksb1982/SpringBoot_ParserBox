4f660fbc7cfae401228932426c7470d33f0186ac
Sergei <Serduyksb@gimai.com>
2021-12-23 14:21:35 +0300

    Create second Test Api. Тест на Junit. 2) получение на вход Entity, с заданными данными и сравнение с ожидаемыми. Использование @MockBean                   private ParserBoxRepository parserBoxRepository;

--- parserbox/src/test/java/ru/serdyuk/parserbox/ParserBoxServiceTest.java
+++ parserbox/src/test/java/ru/serdyuk/parserbox/ParserBoxServiceTest.java
@@ -3,18 +3,41 @@ package ru.serdyuk.parserbox;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.serdyuk.parserbox.api.response.ParserResponse;
+import ru.serdyuk.parserbox.dao.ParserBoxEntity;
+import ru.serdyuk.parserbox.dao.ParserBoxRepository;
 import ru.serdyuk.parserbox.exception.NotFoundException;
 import ru.serdyuk.parserbox.service.ParserBoxService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class ParserBoxServiceTest {
     @Autowired
     private ParserBoxService parserBoxService;
 
+    @MockBean
+    private ParserBoxRepository parserBoxRepository;
+
     @Test
     public void notExistHash() {
         assertThrows(NotFoundException.class, () -> parserBoxService.getByHash("dsfldfsdf"));
+    }
+
+    @Test
+    public void getExistsHash() {
+        ParserBoxEntity entity = new ParserBoxEntity();
+        entity.setHash("1");
+        entity.setData("11");
+        entity.setPublic(true);
+
+        when(parserBoxRepository.getByHash("1")).thenReturn(entity);
+
+        ParserResponse expected = new ParserResponse("11",true);
+        ParserResponse actual = parserBoxService.getByHash("1");
+        assertEquals(expected,actual);
     }
 }
