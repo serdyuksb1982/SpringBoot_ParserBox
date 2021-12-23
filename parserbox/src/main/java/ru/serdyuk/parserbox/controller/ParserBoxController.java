7a01711610a83e24fa938ee234d909177e3b4c3b
Sergei <Serduyksb@gimai.com>
2021-12-23 09:38:17 +0300

    Simple Controller and Api.request init

--- parserbox/src/main/java/ru/serdyuk/parserbox/controller/ParserBoxController.java
+++ parserbox/src/main/java/ru/serdyuk/parserbox/controller/ParserBoxController.java
@@ -1,7 +1,29 @@
 package ru.serdyuk.parserbox.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+
 /**
- * controller
+ * Simple controller
  */
+@RestController
 public class ParserBoxController {
+
+    @GetMapping("/")
+    public Collection<String> getPublicParserList() {
+        return Collections.emptyList();
+    }
+
+    @GetMapping("/{hash}")
+    public String getByHash(@PathVariable String hash) {
+        return hash;
+    }
+
+    /*@PostMapping("/")
+    public String add()*/
 }
